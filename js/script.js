ec9496616dc2f4e45eac7da03684bf9597507736
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-10-25 15:20:44 -0400

    steps in exercise 2.0

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = '2 + 2';
+document.write(simpleAddition);
+
+alert('hello world');
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+// document.write(doubleSize);
+document.write(minSize);
+
+let favoriteFood = 'pizza';
+document.write(favoriteFood);
+
+//note
