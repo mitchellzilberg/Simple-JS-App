dac5e7452dad1de5ecba008a3aa25a74bf175874
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-10-26 14:35:51 -0400

    Added forloop for list + extra
    also changed pokemons heights

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+
 let pokemonList = [
-  {name:'Pikachu', height:5, type:['electric', 'normal']},
-  {name:'Bulbasaur', height:6, type:['grass', 'poison']},
-  {name:'Charmander', height:7, type:['fire', 'flying']}
+  {name:'Pikachu', height:2, type:['electric', 'normal']},
+  {name:'Bulbasaur', height:8, type:['grass', 'poison']},
+  {name:'Charizard', height:15, type:['fire', 'flying']}
 ];
+
+//Lists all the pokemon with their heights. Adds comment if pokemon is big.
+for (let i=0; i <pokemonList.length; i++){
+  if (pokemonList[i].height > 10) {
+  document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height+') -Wow, thats big!');
+  document.write('<br>');
+} else {
+  document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height+') ');
+  document.write('<br>');
+  }
+}
