a924574a7f22d065fe4bcd13fc31ec81931300b8
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-10-28 10:57:30 -0400

    foreachloop + iife
    -Changed forloop to foreachloop
    -put array in iife with return and add functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
+//Pokemon list and functions in IIFE
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name:'Balbasaur', height: 6, type:['grass', 'poison']},
+    {name:'Squirtle', height: 15, type:['water']},
+    {name:'Pikachu', height: 5, type:['electric']},
+    {name:'Charmander', height: 12, type:['fire', 'flying']},
+  ];
+  //add pokemon to pokemonList function if typeof is object
+    function add(pokemon) {
+      if (typeof pokemon === 'object'
+      // && Object.keys(pokemonList) === true  **NOT WORKING NEED HELP**
+    ) {
+        pokemonList.push(pokemon);
+      } else {
+        return 0;
+        console.log('Invalid Pokemon');
+      }
+    }
+  //retieve pokemon from pokemonList function
+    function getAll() {
+      return pokemonList;
+    }
+  //returns functions
+    return {
+      add: add,
+      getAll: getAll,
+  };
+})();
 
-let pokemonList = [
-  {name:'Pikachu', height:2, type:['electric', 'normal']},
-  {name:'Bulbasaur', height:8, type:['grass', 'poison']},
-  {name:'Charizard', height:15, type:['fire', 'flying']}
-];
+pokemonRepository.add({name:'pichu', height: 12});
+console.log(pokemonRepository.getAll());
 
-//Lists all the pokemon with their heights. Adds comment if pokemon is big.
-for (let i=0; i <pokemonList.length; i++){
-  if (pokemonList[i].height > 10) {
-  document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height+') -Wow, thats big!');
-  document.write('<br>');
+//retrieves all items from pokemonList and passes them through foreach loop
+pokemonRepository.getAll().forEach(pokemonListFunction);
+
+//adds comment to each big pokemon and places them on their own lines
+function pokemonListFunction(pokemon) {
+  if (pokemon.height >= 10) {
+  document.write('<p>' + pokemon.name + ', Height:' + pokemon.height + ', Type:' + pokemon.type + ' -Wow, thats big! </p>');
 } else {
-  document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height+') ');
-  document.write('<br>');
-  }
+  document.write('<p>' + pokemon.name + ', Height:' + pokemon.height + ', Type:' + pokemon.type + '</p>');
+}
+  console.log(pokemon.name + ', Height:' + pokemon.height + ', Type:' + pokemon.type);
 }
